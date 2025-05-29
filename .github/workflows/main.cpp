dd3feee10f83cc1f43aa4c6344a5072d7206ac80
abusallat <abusallat1@gmail.com>
2025-05-29 11:52:19 +0500

    Update main.cpp

--- .github/workflows/main.cpp
+++ .github/workflows/main.cpp
@@ -3,11 +3,11 @@
 int main() {
     int a, b;
     
-    std::cout << "Enter two numbers: ";
-    std::cin >> a >> b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
     
-    std::cout << "Addition: " << (a + b) << std::endl;
-    std::cout << "Subtraction: " << (a - b) << std::endl;
+    cout << "Addition: " << (a + b) << std::endl;
+    cout << "Subtraction: " << (a - b) << std::endl;
 
     return 0;
 }
